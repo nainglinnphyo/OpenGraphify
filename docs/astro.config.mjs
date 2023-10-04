e4bcbf1858b3a46da5b4de8a1fc618b901188eea
Naing Linn Phyo <nainglinnphyo24@gmail.com>
2023-10-04 18:19:42 +0630

    feat(app): change serverlesss to static

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
